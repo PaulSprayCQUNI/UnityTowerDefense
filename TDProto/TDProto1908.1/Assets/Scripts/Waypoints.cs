ca7c38b0e33e7babeaabe2f11f565eaf615adc6d
PaulSpray <cr?cr@fRAto_o*a8r9=5>
2019-08-19 13:09:12 +1000

    19Aug1309

---
+++ TDProto/TDProto1908.1/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+   //an array of game objects for Waypoints
+   public static Transform[] points;
+
+   //load the points into the Transform [] array
+   void Awake ()
+   {
+	points = new Transform[transform.childCount];
+	for (int i = 0; i < points.Length; i++) 
+	{
+		points[i]	= transform.GetChild(i);
+	}
+
+   }
+   
+
+}
