24080fd761c8eb4a866defadc2db69fe8f700d06
PaulSpray <cr?cr@fRAto_o*a8r9=5>
2019-09-04 15:00:42 +1000

    4Sep 1500
    4SepCommit

---
+++ TDProto/TDProto1908.1/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// use instatiate to spawn 
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+	public Transform spawnPoint;
+
+	public float waveInterval = 5f;
+	private float countdown = 2f;
+
+	private int waveIndex = 0;
+
+	void Update()
+	{
+		if (countdown <= 0f)
+		{
+			StartCoroutine(SpawnWave());
+			countdown = waveInterval;
+		}
+
+		countdown -= Time.deltaTime;
+	}
+	
+	IEnumerator SpawnWave()
+	{
+	Debug.Log("Wave Incoming");
+	for (int i= 0; i < waveIndex; i++) 
+		{
+			SpawnEnemy();
+			yield return new WaitForSeconds(0.5f);
+		}	
+
+		waveIndex++;
+	}
+	
+	void SpawnEnemy()
+	{
+		Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+	
+
+}
--- TDProto/TDProto1908.1/Assets/Scripts/Waypoints.cs
+++ TDProto/TDProto1908.1/Assets/Scripts/Waypoints.cs
@@ -11,7 +11,7 @@ public class Waypoints : MonoBehaviour
 	points = new Transform[transform.childCount];
 	for (int i = 0; i < points.Length; i++) 
 	{
-		points[i]	= transform.GetChild(i);
+		points[i] = transform.GetChild(i);
 	}
 
    }
