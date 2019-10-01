1886a03c964a831d4e6fdfe4bacf9755befc7dcf
PaulSpray <cr?cr@fRAto_o*a8r9=5>
2019-10-01 15:17:03 +1000

    Oct1 1516
    Ep11 for shop next
    Fix animations and particle effects based on feedback

--- TDProto/TDProto1908.1/Assets/Scripts/Bullet.cs
+++ TDProto/TDProto1908.1/Assets/Scripts/Bullet.cs
@@ -8,6 +8,7 @@ public class Bullet : MonoBehaviour
 
     public float bulletSpeed = 50f;
     public GameObject bulletImpactEffect;
+	public float missileExplosionRadius = 0f;
     public void TargetSeek(Transform _target)
     {
 
@@ -34,6 +35,8 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
+
 
     }
 
@@ -42,7 +45,44 @@ public class Bullet : MonoBehaviour
         GameObject effectInstance = (GameObject)Instantiate(bulletImpactEffect, transform.position, transform.rotation);
         Debug.Log("Our weapons are supremely effective against them Captain!");
         Destroy(effectInstance, 2f);
-        Destroy(target.gameObject);
+
+		if (missileExplosionRadius > 0f)
+		{
+			ExplodeEffect();
+		}
+		else 
+		{
+			DamageEnemy(target);
+		}
+		        
         Destroy(gameObject);
     }
+
+	void ExplodeEffect()
+	{
+	 	Collider[] missileCollisions = Physics.OverlapSphere(transform.position, missileExplosionRadius);
+		foreach (Collider collider in missileCollisions)
+		{
+			if (collider.tag == "Enemy")
+			{
+				DamageEnemy(collider.transform);
+			}
+			
+		}
+	}
+
+	void DamageEnemy(Transform enemy)
+	{
+
+		Destroy(enemy.gameObject);
+	
+	}
+
+	void onDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, missileExplosionRadius);
+	}
+
 }
+
